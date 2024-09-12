986b49b9f0486bc90844419c63698e85c9c22090
Lobo <hagemo83@gmail.com>
2024-09-12 08:36:24 +0200

    2.0
    2.0

--- app.js
+++ app.js
@@ -1,204 +1,102 @@
 /*
-    Twitch Chat Bot Med WebSocket
+    Twitch Chat Client WebSocket WSS
 
     https://github.com/lobossl
 */
 
-let addChat = document.getElementById("addChat")
-let inputChat = document.getElementById("inputChat")
-let btnChat = document.getElementById("btnChat")
-let inputChannel = document.getElementById("inputChannel")
-let inputOauth = document.getElementById("inputOauth")
 let connect = document.getElementById("connect")
-let systemMessages = document.getElementById("systemMessages")
-let btnPause = document.getElementById("btnPause")
+let main = document.getElementById("main")
+let chatWindow = document.getElementById("chatWindow")
+let chatText = document.getElementById("chatText")
+let newStreamer = document.getElementById("newStreamer")
+let newAuth = document.getElementById("newAuth")
 
-let settings = {
-    server: "wss://irc-ws.chat.twitch.tv:443",
-    pause: false
-}
+let server = "wss://irc-ws.chat.twitch.tv:443"
 
 let socket
 
-connect.addEventListener("click",() => {
-    systemMessages.innerText = "Reconnecting.."
-    connectWebSocket()
+connect.addEventListener("click",() =>{
+    connectWebSocket(newStreamer.value,newAuth.value)
+    newStreamer.style.display = "none"
+    newAuth.style.display = "none"
+    connect.style.display = "none"
 })
 
-function connectWebSocket() {
-    socket = new WebSocket(settings.server)
+function connectWebSocket(newStreamer,newAuth) {
+    socket = new WebSocket(server)
     
     socket.addEventListener("close",() => {
-        systemMessages.innerText = "Connection closed #3, reconnecting in 5 seconds"
+        console.log("closed")
         setTimeout(() => {
             connectWebSocket()
         },5000)
     })
     
     socket.addEventListener("error",() => {
-        systemMessages.innerText = "Connection error #4, reconnecting in 5 seconds"
+        console.log("error")
         setTimeout(() => {
             connectWebSocket()
         },5000)
     })
     
     socket.addEventListener("open",() => {
-        if(inputOauth.length > 0)
-        {
-            socket.send("PASS " + inputOauth.value + "\r\n")
-            socket.send("NICK Anonymous3331\r\n")
-            socket.send("JOIN #" + inputChannel.value + "\r\n")
+        console.log("open")
+        if(newAuth){
+            socket.send("PASS " + newAuth + "\r\n")
+            socket.send("NICK kkjk4555555\r\n")
+            socket.send("JOIN #" + newStreamer + "\r\n")
+            addMessage()
         }
-        else
-        {
-            socket.send("NICK justinfan3331\r\n")
-            socket.send("JOIN #" + inputChannel.value + "\r\n")
+        else{
+            socket.send("NICK justinfan3332\r\n")
+            socket.send("JOIN #" + newStreamer + "\r\n")
         }
-
-        systemMessages.innerText = "Connected to " + inputChannel.value
     })
     
-    socket.addEventListener("message", (e) => {
-        let data = e.data.split(" ")
-    
-        if(data[0] === "PING")
-        {
-            console.log("Ping Pong!")
-            socket.send("PONG " +  data[0] + "\r\n")
-        }
-        else
-        {
-            if(data[1] === "PRIVMSG")
-            {
-                let emtyStr = ""
-    
-                let getNick = data[0].split("!",1)[0].split(":",2)[1]
-                let getChannel = data[2]
-                let getMessage = data.slice(3,data.length)
-                let getInfo = data[1]
-    
-                getMessage.forEach((e,index) => {
-                    emtyStr += e + " "
-                })
-    
-                CREATEDIVS(getInfo,getNick,emtyStr,getChannel)
-            }
-            else
-            {
-                console.log(data)
-            }
+    socket.addEventListener("message",(e) => {
+        let data = e.data
+        let regexData = data.split(" ")
+
+        if(regexData[1] === "PRIVMSG"){
+            let username = regexData[0].split(":")[1].split("!")[0]
+            let channel = regexData[2]
+            let message = data.split(":")[2]
+
+            getMessages(channel,username,message)
         }
     })
 }
 
-document.addEventListener("click",(e) => {
-    if(e.target.className === "reply")
-    {
-        inputChat.innerText = "@" + e.target.id
-    }
-    else if(e.target.className === "ban")
-    {
-        inputChat.innerText = "/ban " + e.target.id + " " + "Banned"
-    }
-    else
-    {
-        return null
-    }
-})
+function getMessages(channel,username,message){
+    let chatBoxes = document.createElement("div")
 
-btnPause.addEventListener("click",() => {
-    if(settings.pause)
-    {
-        settings.pause = false
-    }
-    else
-    {
-        settings.pause = true
-    }
-})
+    chatBoxes.id = "chatBoxes"
+    chatBoxes.className = 
+    chatBoxes.innerText = "[#" + channel + "] " + username + ": " + message
 
-btnChat.addEventListener("click",() => {
-    try
-    {
-        socket.send("PRIVMSG #" + inputChannel.value + " :" + inputChat.innerText + "\r\n")
+    chatWindow.append(chatBoxes)
 
-        CREATEDIVS("PRIVMSG","YOU",":" + inputChat.innerText,inputChannel.value)
+    chatWindow.scrollTop = chatWindow.scrollHeight
 
-        inputChat.innerText = ""
-    }
-    catch(err)
-    {
-        systemMessages.innerText = "Error, failed to send message"
-    }
-})
+    chatBoxes.addEventListener("click",() =>{
+        socket.send(`PRIVMSG #${channel} :/timeout ${username} 600 Bye.\r\n`)
+    })
+}
 
-function CREATEDIVS(info,currentNick,currentMessage,currentChannel)
-{
-    let createDiv = document.createElement("div")
-    let createDivIcons = document.createElement("div")
-    let createDivReply = document.createElement("img")
-    let createDivBan = document.createElement("img")
-    let createDivNick = document.createElement("span")
-    let createDivMsg = document.createElement("span")
+function addMessage(){
+    let newMessage = document.createElement("input")
 
-    if(info === "PRIVMSG")
-    {
-        createDivNick.innerText = currentNick
-        createDivMsg.innerText = currentMessage
-    }
-    else
-    {
-        return null
-    }
+    newMessage.id = "newMessage"
+    newMessage.type = "text"
+    newMessage.placeholder = "Message:"
 
-    createDiv.style.border = "1px solid #CCCCCC"
-    createDiv.style.borderRadius = "6px"
-    createDiv.style.wordBreak = "break-word"
-    createDiv.style.padding = "5px"
-    createDiv.style.margin = "5px"
-    createDiv.style.textAlign = "left"
-    createDiv.style.fontFamily = "monospace"
-    createDiv.style.fontSize = "1.2em"
-    createDiv.style.backgroundColor = "#FFFFFF"
+    chatTools.append(newMessage)
 
-    createDivNick.style.color = "orange"
-    createDivNick.style.fontSize = "1.3em"
+    newMessage.addEventListener("keyup",(e) =>{
+        if(e.key === "Enter"){
+            socket.send(`PRIVMSG #${newStreamer.value} :${newMessage.value}\r\n`)
 
-    createDivMsg.style.color = "grey"
-    createDivMsg.style.fontSize = "1em"
-    createDivMsg.style.margin = "5px"
-
-    createDivReply.src = "img/reply.png"
-    createDivReply.id = currentNick
-    createDivReply.className = "reply"
-    createDivReply.style.margin = "2px"
-    createDivReply.style.padding = "2px"
-    createDivReply.style.border = "1px solid #556677"
-    createDivReply.style.borderRadius = "3px"
-    createDivReply.style.cursor = "pointer"
-
-    createDivBan.src = "img/banned.png"
-    createDivBan.id = currentNick
-    createDivBan.className = "ban"
-    createDivBan.style.margin = "2px"
-    createDivBan.style.padding = "2px"
-    createDivBan.style.border = "1px solid #FF0000"
-    createDivBan.style.borderRadius = "3px"
-    createDivBan.style.cursor = "pointer"
-
-    createDivIcons.style.textAlign = "right"
-    createDivIcons.style.borderBottom = "1px solid #CCCCCC"
-
-    createDivIcons.append(createDivReply)
-    createDivIcons.append(createDivBan)
-    createDiv.append(createDivIcons)
-    createDiv.append(createDivNick)
-    createDiv.append(createDivMsg)
-
-    addChat.append(createDiv)
-
-    if(settings.pause == false)
-    {
-        addChat.scrollTop = addChat.scrollHeight
-    }
+            newMessage.value = ""
+        }
+    })
 }
