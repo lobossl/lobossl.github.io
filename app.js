4cf042064853a04500a3ee3b32d4c97065931809
Lobo <hagemo83@gmail.com>
2024-11-11 18:26:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -68,6 +68,7 @@ function loadResults(){
 		DIV_TEXTAREA.innerText = e.textarea
 		DIV_TEXTAREA.className = "textareaDiv"
 
+		DIV_DELETE.innerText = " "
 		DIV_DELETE.className = "deleteDiv"
 
 		DIV_MAIN.append(DIV_DATE)	
