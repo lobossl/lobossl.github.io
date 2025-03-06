cc7c23936559c68c04e558d19c87c96002d3d9cf
Lobo <hagemo83@gmail.com>
2025-03-06 02:46:21 +0100

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-/*
-    Todo:
-    - Hvis man trykker på meny knappen skal det komme opp en meny.
-    - Finn ut åsen jeg vil ha det!
-*/
-document.getElementById("logo_text_click").addEventListener("click",() =>{
-    console.log("yo")
-})
