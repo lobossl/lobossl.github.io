4b5f4d91dc8a08a69c191deb22b30d2c2acd1c2a
Lobo <hagemo83@gmail.com>
2025-03-20 15:13:19 +0100

    3/20/2025
    3/20/2025

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-/* fliro.org */
+/*
+    644a21eda12751316f7f17b2bbd5df4fc11f1471
+    Fliro.org web design  2025
+*/
 let clicked = false
 document.getElementById("menu").addEventListener("click",() =>{
     if(clicked){
@@ -11,6 +14,7 @@ document.getElementById("menu").addEventListener("click",() =>{
         cE.innerHTML = `
             <a href='index.html'>Hjem</a>
             <a href='#kontakt'>Kontakt</a>
+            <a href='http://www.trafikkskilt.no/index.shtml'>Trafikkskilt</a>
         `
         cE.style.width = "100%"
         cE.style.backgroundColor = "#222222"
