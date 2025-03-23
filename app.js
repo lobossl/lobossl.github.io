caaabb1feda751204a1ed0fbcf6cca22a0186fc6
Lobo <hagemo83@gmail.com>
2025-03-23 19:27:12 +0100

    1.0
    1.0

--- app.js
+++ app.js
@@ -2,6 +2,31 @@
     JS BY FLIRO.ORG
 */
 
-let test = new Date().getFullYear()
+let getYear = new Date().getFullYear()
 
-document.getElementById("bottom").innerHTML = `<p>Utviklet av <a href='https://fliro.org'>Fliro.org</a> &#169; ${test}</p>`
+document.getElementById("bottom").innerHTML = `<p>Utviklet av <a href='https://fliro.org'>Fliro.org</a> &#169; ${getYear}</p>`
+
+let clicked = false
+
+document.getElementById("btn_menu").addEventListener("click",() =>{
+    document.getElementById("btn_menu").innerText = "="
+
+    if(clicked === false){
+        document.getElementById("btn_menu").innerText = ""
+
+        let newDiv = document.createElement("span")
+
+        newDiv.innerHTML = `
+            <a href='#kontakt'>kontakt</a>
+        `
+        newDiv.style.textAlign = "center"
+        newDiv.style.padding = "15px"
+
+        document.getElementById("btn_menu").appendChild(newDiv)
+
+        clicked = true
+    }
+    else{
+        clicked = false
+    }
+})
